252167ed3b7d966ee9f01f56f6e7277659fb1a30
Cloverk1t <cloverkit@outlook.com>
2020-10-17 15:39:21 +0800

    append HomeFragment

--- app/src/main/java/com/seclab/nmaping/ui/home/HomeFragment.java
+++ app/src/main/java/com/seclab/nmaping/ui/home/HomeFragment.java
@@ -1,18 +1,22 @@
 package com.seclab.nmaping.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.DhcpInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.seclab.nmaping.R;
+import com.seclab.nmaping.content.MyApplication;
 
 public class HomeFragment extends Fragment {
 
@@ -20,16 +24,42 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+//        homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        final TextView text_network = root.findViewById(R.id.text_network);
+        final TextView text_security = root.findViewById(R.id.text_security);
+        final TextView text_bssid = root.findViewById(R.id.text_bssid);
+        final TextView text_dhcp = root.findViewById(R.id.text_dhcp);
+        final TextView text_ipAddress = root.findViewById(R.id.text_ip);
+        final TextView text_netmask = root.findViewById(R.id.text_netmask);
+        final TextView text_gateway = root.findViewById(R.id.text_gateway);
+        final TextView text_dns1 = root.findViewById(R.id.text_dns1);
+        final TextView text_dns2 = root.findViewById(R.id.text_dns2);
+        final Button button_scan = root.findViewById(R.id.btnScan);
+        final Button button_settings = root.findViewById(R.id.btnSettings);
+        button_scan.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), HomeFragment.class));
         });
+        button_settings.setOnClickListener(v -> {
+            startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+        });
+        @SuppressLint("WrongConstant") WifiManager mWifiManger = (WifiManager) MyApplication.getmContText().getSystemService("wifi");
+        DhcpInfo dhcpInfo = mWifiManger.getDhcpInfo();
+        text_network.setText(mWifiManger.getConnectionInfo().getSSID().toString());
+        text_bssid.setText(mWifiManger.getConnectionInfo().getBSSID().toString());
+        text_dhcp.setText(intToIp(dhcpInfo.serverAddress));
+        text_ipAddress.setText(intToIp(dhcpInfo.ipAddress));
+        text_netmask.setText(intToIp(dhcpInfo.netmask));
+        text_gateway.setText(intToIp(dhcpInfo.gateway));
+        text_dns1.setText(intToIp(dhcpInfo.dns1));
+        text_dns2.setText(intToIp(dhcpInfo.dns2));
         return root;
     }
+
+    public String intToIp(int paramInt) {
+        return (paramInt & 0xFF) + "." + (0xFF & paramInt >> 8) + "." + (0xFF & paramInt >> 16) + "."
+                + (0xFF & paramInt >> 24);
+    }
 }
+
--- app/src/main/java/com/seclab/nmaping/utils/IpUtils.java
+++ app/src/main/java/com/seclab/nmaping/utils/IpUtils.java
@@ -29,4 +29,5 @@ public class IpUtils {
 
         return match.find();
     }
+
 }
